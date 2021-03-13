a32fe2cd8ba8f21c8dcd70b4cdd3fb38d98561f7
Greg Brosman <greg.brosman@gmail.com>
2021-03-12 18:15:46 -0800

    more work on problem 7

--- problem7.py
+++ problem7.py
@@ -1,23 +1,19 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
-def prime_numbers():  
+def prime_numbers(index):
     primes = []
 
-    # while each iteration is less than or equal to the passed in number...
-    for num in range(2, ):
-        if num > 1:
-            for i in range(2, num):
-
-                # If the passed in number divided by the current iteration has a remainder of 0...
-                if num % i == 0:
-                    break
-            else:
-                primes.append(num)
-    print(primes)
-
-    # Need generator
-                           
-                
+    while len(primes) < index: 
+        for num in range(2, index ** 2):
+            if num > 1:
+                for i in range(2, num):
+                    if num % i == 0:
+                        break
+                else:
+                    primes.append(num)
     
-prime_numbers()
+    print(len(primes))    
+    print(primes[index])            
+    
+prime_numbers(1000)
