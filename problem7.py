dfb3ea3333b4ada9332223d116e907efd67d8cbc
Greg Brosman <greg.brosman@gmail.com>
2021-03-15 16:01:13 -0700

    problem 7 correct

--- problem7.py
+++ problem7.py
@@ -1,19 +1,18 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-# What is the 10 001st prime number?
+# What is the 10,001st prime number?
 
-def prime_numbers(index):
-    primes = []
 
-    while len(primes) < index: 
-        for num in range(2, index ** 2):
-            if num > 1:
-                for i in range(2, num):
-                    if num % i == 0:
-                        break
-                else:
-                    primes.append(num)
-    
-    print(len(primes))    
-    print(primes[index])            
-    
-prime_numbers(1000)
+def nth_prime(n):
+    counter = 2
+    for i in range(3, n**2, 2):
+        k = 1
+        while k*k < i:
+            k += 2
+            if i % k == 0:
+               break
+        else:
+            counter += 1
+        if counter == n:
+            return i
+
+print(nth_prime(10001))
